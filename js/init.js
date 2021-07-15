dfbf5cd40cc3246d048e30fa0d57c06064b78ded
AleHersheys <67510933+AleHersheys@users.noreply.github.com>
2021-07-14 19:06:42 -0500

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems);
+
+  var elems = document.querySelectorAll('.slider');
+  var instances = M.Slider.init(elems, {
+    indicators:false,
+    height:500,
+    duration:500,
+    interval:5000
+  });
+});
